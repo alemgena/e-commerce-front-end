51643011a50b0bd4250e6084cdbe72c2370a0109
Robel shewangzaw <=>
2022-12-30 20:47:34 +0300

    minor changes

--- mock/products.js
+++ mock/products.js
@@ -55,32 +55,4 @@ export const products = [
     _type: 'product',
     _updatedAt: '2022-06-29T13:16:46Z',
   },
-  {
-    brand: 'JBL',
-    category: ['digital', 'other', 'speaker'],
-    details: {
-      bluetooth: true,
-      connectionType: ['Bluetooth'],
-      depth: '13.2 cm',
-      frequency_response: ' 55Hz–20kHz',
-      height: '13.6 cm',
-      width: ' 28.8 cm',
-    },
-    image: ['/images/house-banner.webp', '/images/house-banner.webp'],
-    isOffer: true,
-    name: 'JBL Xtreme 2 Rugged/Waterproof Bluetooth Wireless Speaker - Black',
-    price: 328.12,
-    registerDate: '2022-06-29T13:16:36.873Z',
-    slug: {
-      current:
-        'jbl-xtreme-2-rugged-waterproof-bluetooth-wireless-speaker-black',
-      _type: 'slug',
-    },
-    starRating: 4,
-    _createdAt: '2022-06-29T13:16:46Z',
-    _id: '063c1752-10b6-48c5-be61-6ff074c58640',
-    _rev: 'MIBn063RYGY6Oom8VmUr8N',
-    _type: 'product',
-    _updatedAt: '2022-06-29T13:16:46Z',
-  },
-];
+  
