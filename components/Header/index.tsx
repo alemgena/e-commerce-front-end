17e4ca1c30fa75c8db2185caaeac67b295ff39f9
Robel shewangzaw <=>
2023-01-11 10:20:22 +0300

    implementing the chat story

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable jsx-a11y/alt-text */
+import Link from 'next/link';
+
 import { NavItems } from './nav-items';
 import { SearchBar } from './search-bar';
 
 export function Header() {
   return (
-    <div className="flex w-full items-center justify-between bg-white px-12 py-7">
+    <div className=" flex w-full items-center justify-between bg-white px-12 py-7">
       <div className="flex items-center gap-16">
-        <img src="/images/logo.svg" className="h-14 w-16" />
+        <Link href="/">
+          <img src="/images/logo.svg" className="h-14 w-16" />
+        </Link>
+
         <SearchBar />
       </div>
       <NavItems />
