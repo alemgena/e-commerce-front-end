fee6c8c0ff66cbd9e69ecbf83042a91683ded873
alemgena <alemgenateferi1@gmil.com>
2023-03-15 10:38:00 +0300

    fix minor issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,12 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   providers: [
     Providers.Google({
-      clientId: process.env.clientId,
-      clientSecret: process.env.clientSecret,
-      callbackURL:'https://liyumarket.com/auth/google/callback',
-      scope:'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+      clientId:
+        '102271526435-c4eiv23m7ph99fj6audirufk8lh8gefl.apps.googleusercontent.com',
+      clientSecret: 'GOCSPX - a73PAmLqV3MyuXrwjykLJwUECsmX',
+      callbackURL: 'https://liyumarket.com/auth/google/callback',
+      scope:
+        'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
     }),
   ],
   callbacks: {
