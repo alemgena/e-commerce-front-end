d794a87f1edfb9c72babb2ad7465f2fe0c0746f5
alemgena <alemgenateferi1@gmil.com>
2023-05-25 16:44:55 +0300

    add

--- apis/product.tsx
+++ apis/product.tsx
@@ -3,6 +3,6 @@ import { baseURL } from '@/config';
 export const getProductsAPI = async () => axios.get(`${baseURL}api/products`)
 export const getProductsByFeaturedAPI = async () =>
   axios.get(
-    `${baseURL}api/products`
+    `${baseURL}api/products/list`
   );
 export const getProductAPI = async (id:string) => axios.get(`${baseURL}api/products/${id}`);
