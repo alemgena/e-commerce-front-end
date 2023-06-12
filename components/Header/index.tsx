bea6f7ae1c3119f51b489ed384afe94c89a66167
alemgena <alemgenateferi1@gmil.com>
2023-06-12 11:00:09 +0300

    change the logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -13,38 +13,12 @@ export function Header() {
   return (
     <div className="z-100  fixed sticky left-0 right-0 top-0 top-0 z-50 w-full bg-white bg-white shadow-lg">
       <div className="container m-auto flex w-full items-center justify-between bg-white py-2">
-        {largeDevice ? (
-          <div className="flex">
-            <Link href="/">
-              <img
-                src="/images/logo.svg"
-                className="h-14 w-16 cursor-pointer lg:ml-4"
-              />
-            </Link>
-            <span className="ml-4 mt-3">
-              <div
-                className="font-bold"
-                style={{ color: '#000000', fontSize: '15px' }}
-              >
-                Liymarkiet
-              </div>
-              <div
-                className="font-bold"
-                style={{ color: '#000000', fontSize: '15px' }}
-              >
-                ልዩ ማርኬት
-              </div>
-            </span>
-          </div>
-        ) : (
           <Link href="/">
             <img
-              src="/images/logo.svg"
-              className="h-14 w-16 cursor-pointer lg:ml-4"
+              src="/images/newLogo.jpg"
+              className="h-14 w-20 lg:w-24 cursor-pointer lg:ml-2"
             />
           </Link>
-        )}
-
         <SearchBar />
         <NavItems />
       </div>
